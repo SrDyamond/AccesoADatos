4cd21da41b6279f49da8008df9019869af9e8ec8
Dyango <dsanchezr@fpcoruna.afundacion.org>
2020-10-05 18:01:15 +0200

    "#Tarea6-WorkInProgress"

---
+++ 0006-#Tarea6/src/com/afundacionfp/LecturaXml.java
@@ -0,0 +1,16 @@
+package com.afundacionfp;
+
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class LecturaXml {
+    public static void main(String args[]) {
+
+
+    }
+}
