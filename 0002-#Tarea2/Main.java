6a7ae8d7e16c72b25b46341f9bd76e021122ba46
Dyango <dsanchezr@fpcoruna.afundacion.org>
2020-10-05 17:17:02 +0200

    "#Tarea2"

--- 0002-#Tarea2/Main.java
+++ 0002-#Tarea2/Main.java
@@ -17,6 +17,7 @@ public class Main {
         while (input.equals("salir") == false) {
             System.out.println("Teclea un nombre o 'salir' para terminar");
             input = scanner.nextLine();
+
             System.out.println("Tecleaste: " + input);
             System.out.println("Ahora se va a guardar al fichero...");
             fileWriter.write(input);
