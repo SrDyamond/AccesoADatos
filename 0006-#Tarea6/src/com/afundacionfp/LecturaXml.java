d17833bf0419197d00c95c6d964d4fc67c9cca9e
Dyango <dsanchezr@fpcoruna.afundacion.org>
2020-10-05 18:37:24 +0200

    "#Tarea6-WorkInProgress"

--- 0006-#Tarea6/src/com/afundacionfp/LecturaXml.java
+++ 0006-#Tarea6/src/com/afundacionfp/LecturaXml.java
@@ -10,7 +10,27 @@ import org.w3c.dom.NodeList;
 
 public class LecturaXml {
     public static void main(String args[]) {
-
-
+        try {
+            File archivo = new File("C:\\Users\\Lenovo\\Desktop\\ListaViajes.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
+            Document document = documentBuilder.parse(archivo);
+            document.getDocumentElement().normalize();
+            System.out.println("Elemento raiz:" + document.getDocumentElement().getNodeName());
+            NodeList viajes = document.getElementsByTagName("empleado");
+            for (int temp = 0; temp < viajes.getLength(); temp++) {
+                Node nodo = viajes.item(temp);
+                System.out.println("Elemento:" + nodo.getNodeName());
+                if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) nodo;
+                    System.out.println("id: " + element.getAttribute("id"));
+                    System.out.println("Nombre: " + element.getElementsByTagName("nombre").item(0).getTextContent());
+                    System.out.println("username: " + element.getElementsByTagName("username").item(0).getTextContent());
+                    System.out.println("password: " + element.getElementsByTagName("321423").item(0).getTextContent());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
