e63208e1a7dbc3e1f6e79d446a50c744545db9d9
Dyango <dsanchezr@fpcoruna.afundacion.org>
2020-10-06 11:41:10 +0200

    "#Tarea6-WorkInProgress"

--- 0006-#Tarea6/src/com/afundacionfp/Main.java
+++ 0006-#Tarea6/src/com/afundacionfp/Main.java
@@ -8,25 +8,27 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class LecturaXml {
+public class Main {
     public static void main(String args[]) {
         try {
-            // File archivo = new File("C:\\Users\\Lenovo\\Desktop\\ListaViajes.xml");
-            File archivo = new File("C:\\Users\\dyang\\OneDrive\\Escritorio\\ListaViajes.xml");
+             File archivo = new File("C:\\Users\\Lenovo\\Desktop\\ListaViajes.xml");
+           // File archivo = new File("C:\\Users\\dyang\\OneDrive\\Escritorio\\Viajes.xml");
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
             Document document = documentBuilder.parse(archivo);
             document.getDocumentElement().normalize();
             System.out.println("Elemento raiz:" + document.getDocumentElement().getNodeName());
-            NodeList viaje = document.getElementsByTagName("viaje");
-            for (int temp = 0; temp < viaje.getLength(); temp++) {
-                Node nodo = viaje.item(temp);
+            NodeList Viajes = document.getElementsByTagName("Viaje");
+            for (int temp = 0; temp < Viajes.getLength(); temp++) {
+                Node nodo = Viajes.item(temp);
                 System.out.println("Elemento:" + nodo.getNodeName());
                 if (nodo.getNodeType() == Node.ELEMENT_NODE) {
                     Element element = (Element) nodo;
                     System.out.println("CiudadOrigen: " + element.getElementsByTagName("CiudadOrigen").item(0).getTextContent());
                     System.out.println("CiudadDestino: " + element.getElementsByTagName("CiudadDestino").item(0).getTextContent());
-                    System.out.println("ListaParadas: " + element.getElementsByTagName("ListaParadas").item(0).getTextContent());
+                    if ((element.getElementsByTagName("ListaParadas").item(0).getTextContent()) != null));{
+                        System.out.println("ListaParadas: " + element.getElementsByTagName("ListaParadas").item(0).getTextContent());
+                    }
                     System.out.println("Parada: " + element.getElementsByTagName("Parada").item(0).getTextContent());
                     System.out.println("Ciudad: " + element.getElementsByTagName("Ciudad").item(0).getTextContent());
                     System.out.println("TiempoDescanso: " + element.getElementsByTagName("TiempoDescanso").item(0).getTextContent());
