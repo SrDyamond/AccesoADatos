1239d1f3268efb443a518ef23dec27e29e747cbe
Dyango <dsanchezr@fpcoruna.afundacion.org>
2020-10-19 18:14:23 +0200

    "BoletinJava.Ejercicio 2.3.Se usa @override para sobrescricir el toString()"

--- BoletinJavaExtra/src/com/afundacionfp/Main.java
+++ BoletinJavaExtra/src/com/afundacionfp/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         Persona instanciaPersona = new Persona("Agustín", "Gutiérrez Beira");
         System.out.println(instanciaPersona);
-
     }
 }
