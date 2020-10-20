3d2caf121bb1567f37e6f8e8cbfe4756012d94c5
SrDyamond <dyangosanchezrodriguez@gmail.com>
2020-10-20 11:52:10 +0200

    "Ejercico 3.1.Que los numeros son distintos"

--- BoletinJavaExtra/src/com/afundacionfp/Main.java
+++ BoletinJavaExtra/src/com/afundacionfp/Main.java
@@ -6,5 +6,16 @@ public class Main {
         Persona instanciaPersona = new Persona("Agustín", "Gutiérrez Beira");
         System.out.println(instanciaPersona);
 
+
+
+        int numero1 = 12;
+        int numero2 = 13;
+        if (numero1 == numero2) {
+            System.out.println("Son el mismo número");
+        } else {
+            System.out.println("Son distintos números");
+        }
+
+
     }
 }
