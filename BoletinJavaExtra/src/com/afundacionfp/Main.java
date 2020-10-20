0558291490b786854f1ae37aa45f50a772e4116c
SrDyamond <dyangosanchezrodriguez@gmail.com>
2020-10-20 12:03:10 +0200

    "Ejercico 3.3.  Porque hemos cambio el nombre de un objecto.Y persona 2 contiene lo mismo  que persona 1.
     ¿De qué sirve cuando se usa en objetos Java? ¿Qué "compara" realmente?
     Compara el objeto en si.
    "

--- BoletinJavaExtra/src/com/afundacionfp/Main.java
+++ BoletinJavaExtra/src/com/afundacionfp/Main.java
@@ -4,9 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         Persona persona1 = new Persona("Agustín", "Gutiérrez Beira");
+        Persona persona2 = persona1;
         System.out.println(persona1);
+        System.out.println(persona2);
         persona1.setNombre("Pepe");
         System.out.println(persona1);
+        System.out.println(persona2);
+
 
     }
 }
