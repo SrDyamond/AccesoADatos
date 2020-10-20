5f6678bfec96acbbb460dd0754590e12cc6f0e60
SrDyamond <dyangosanchezrodriguez@gmail.com>
2020-10-20 13:43:08 +0200

    "Ejercicio 4.5. Inicializado como arryaList."

--- BoletinJavaExtra/src/com/afundacionfp/Main.java
+++ BoletinJavaExtra/src/com/afundacionfp/Main.java
@@ -1,8 +1,14 @@
 package com.afundacionfp;
-
+import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList persona = new ArrayList();
+        persona.add("Agustin");
+        persona.add("Jaime");
+        persona.add("Laura");
+        System.out.println(persona);
+        /*
         Persona arrPersona[] = new Persona[3];
 
         arrPersona[0] = new Persona("Agustin","Gutierrez Beira");
@@ -12,7 +18,7 @@ public class Main {
         for(int i = 0; i< arrPersona.length; i++) {
             System.out.println(arrPersona[i]);
         }
-/*
+
         if (arrPersona[0].equals(arrPersona[1])) {
             System.out.println("Tienen el mismo nombre y apellidos");
         } else {
