ce68d370081c8a8934bef73726ebc6021924453a
SrDyamond <dyangosanchezrodriguez@gmail.com>
2020-10-20 13:48:14 +0200

    "Ejercicio 4.6"

--- BoletinJavaExtra/src/com/afundacionfp/Main.java
+++ BoletinJavaExtra/src/com/afundacionfp/Main.java
@@ -4,21 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList persona = new ArrayList();
-        persona.add("Agustin");
-        persona.add("Jaime");
-        persona.add("Laura");
-        System.out.println(persona);
+        persona.add("Agustin Gutierrez Beira ");
+        persona.add("Jaime Gutierrez Beira");
+        persona.add("Laura Dominguez Caballero");
+       // System.out.println(persona);
         /*
         Persona arrPersona[] = new Persona[3];
 
         arrPersona[0] = new Persona("Agustin","Gutierrez Beira");
         arrPersona[1] = new Persona("Jaime","Gutierrez Beira");
         arrPersona[2] = new Persona("Laura","Dominguez Caballero");
-
-        for(int i = 0; i< arrPersona.length; i++) {
-            System.out.println(arrPersona[i]);
+*/
+        for(int i = 0; i< persona.size() ; i++) {
+            System.out.println(persona.get(i));
         }
-
+/*
         if (arrPersona[0].equals(arrPersona[1])) {
             System.out.println("Tienen el mismo nombre y apellidos");
         } else {
