1618d1480da7e4bcbcfe7e54cd5dc916118a96b4
SrDyamond <dyangosanchezrodriguez@gmail.com>
2020-10-20 11:49:54 +0200

    "Ejercico 2.4:Salen los apellidos y el nombre por pantalla."

--- 0007-#Tarea7/src/com/afundacionfp/Main.java
+++ 0007-#Tarea7/src/com/afundacionfp/Main.java
@@ -18,9 +18,7 @@ public class Main {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
 
-
-        File inputFile = new File("C:\\Users\\Lenovo\\Desktop\\lista_viajes.xml");
-        //File inputFile = new File("C:\\Users\\dyang\\OneDrive\\Escritorio\\lista_viajes.xml");
+        File inputFile = new File("C:\\Users\\dyang\\OneDrive\\Escritorio\\lista_viajes.xml");
         Document doc = builder.parse(new FileInputStream(inputFile));
 
         ListaViajes listaViajes = null;
@@ -30,8 +28,7 @@ public class Main {
             e.printStackTrace();
         }
 
-        File outputFile = new File("C:\\Users\\Lenovo\\Desktop\\lista_viajes.json");
-        //File outputFile = new File("C:\\Users\\dyang\\OneDrive\\Escritorio\\lista_viajes.json");
+        File outputFile = new File("C:\\Users\\dyang\\OneDrive\\Escritorio\\lista_viajes.json");
         FileWriter fileWriter = new FileWriter(outputFile);
         // La siguiente línea guarda el JSON en el archivo.
         // No importa que esté todo en la misma línea, sigue siendo un JSON válido.
