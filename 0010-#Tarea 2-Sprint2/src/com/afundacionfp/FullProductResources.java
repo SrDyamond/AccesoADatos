df4ed557d02330d3f893adca9e99d4955c4bfc6f
SrDyamond <dyangosanchezrodriguez@gmail.com>
2020-10-31 16:44:43 +0100

    "#12-Tarea4:Update"

--- 0010-#Tarea 2-Sprint2/src/com/afundacionfp/FullProductResources.java
+++ 0010-#Tarea 2-Sprint2/src/com/afundacionfp/FullProductResources.java
@@ -11,11 +11,7 @@ public class FullProductResources extends ServerResource {
         DataProvider dataProvider = new MockDataProvider();
         //Meto en la variable referencia el valor del parametro
         String reference = (String) getRequest().getAttributes().get("reference");
-        // Creo un objeto JSONArray
-        JSONArray jsonArray = new JSONArray();
-       for (Product product: dataProvider.getFullProduct()) {
-         jsonArray.add(product.toJSOn());
-       }
-        return jsonArray.toJSONString();
+
+        return dataProvider.getFullProduct(reference).toJSOn().toJSONString();
     }
 }
